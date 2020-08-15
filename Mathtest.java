df7747a8801170c4252cb3e19ad774124a362338
lizhaol <lei15231157310@163.com>
2020-08-15 20:50:33 +0800

    first commit

---
+++ Mathtest.java
@@ -0,0 +1,15 @@
+package Test1;
+
+public class Mathtest {
+
+	public static void main(String[] args) {
+		System.out.println(Math.round(11.5));
+		System.out.println(Math.round(-11.5));
+		System.out.println(Math.round(11.3));
+		System.out.println(Math.round(-11.3));
+		System.out.println(Math.round(4.5));
+		System.out.println(Math.round(-4.5));
+
+	}
+
+}
---
+++ shuzu.java
@@ -0,0 +1,21 @@
+package Test1;
+
+public class shuzu {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int a[] = new int[5];
+		for(int it : a) 
+		{
+			System.out.println(it);
+		}
+		
+		int b[][] = new int[5][];
+		for(int[] it : b) 
+		{
+			System.out.println(it);
+		}
+		
+	}
+
+}
